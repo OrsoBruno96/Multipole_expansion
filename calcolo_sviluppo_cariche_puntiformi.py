03fbb5e94d2eb8b7b29613b3fed3430ea09cd8db
U-n551jx-cn043h\user <fabio.zoratti96@gmail.com>
2016-11-16 09:39:51 +0100

    prima versione

---
+++ calcolo_sviluppo_cariche_puntiformi.py
@@ -0,0 +1,156 @@
+import mpmath
+import sympy
+from sympy import symbols
+from sympy import *
+from sympy import init_printing
+from sympy.abc import theta, phi
+from sympy import Integral, latex
+import numpy
+
+x = symbols('x')
+R = symbols('r')
+q = symbols('q')
+a = symbols('a')
+theta = symbols('theta') 
+phi = symbols('phi')
+
+
+
+#############  QUI I DATI DA INSERIRE ############
+
+esempio_1 = numpy.array([[a,0,0,q],[a,sympy.pi,0,q], [a,sympy.pi/2, 0, q], [a,sympy.pi/2, sympy.pi/2, q], [a,sympy.pi/2, sympy.pi, q], [a,sympy.pi/2, 3*sympy.pi/2, q], [0,0,0,-6*q] ]) #6 cariche sugli assi cartesiani a distanza a dal centro e una in mezzo -6q
+esempio_2 = numpy.array([[a,0,0,q],[a,sympy.pi,0,-q]]) #classico dipolo elettrico
+esempio_3 = numpy.array([[a,0,0,q],[a,sympy.pi,0,q], [0,0,0,-2*q]]) #quadrupolo classico
+
+
+
+
+
+vettore_cariche = esempio_2 #in questo caso il vettore è [r,theta,phi, q]
+file_out = 'sviluppo.tex'       # su quale file .tex vuoi scrivere
+ordine = 2                      #ordine a cui fermare lo sviluppo
+
+
+
+
+########## FINE DATI DA INSERIRE ###########
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################
+#####   QUESTO E' IL PROGRAMMA ###########
+##########################################
+
+
+
+
+
+
+def polinomio_Legendre(l):
+    if not (isinstance(l, int)): ## controlla se il valore inserito è un intero
+        print('Solo valori interi per i polinomi di legendre, pota')
+        return
+    y = (x**2-1)**l
+    for i in range(0,l):
+        y = diff(y, x)        
+    y = y *Rational(1/ (2**l*factorial(l))) 
+    y = factor(y)   
+    return y
+    
+    
+def polinomio_Legendre_associato(l,m):
+    if not (isinstance(l, int)):
+        print('l deve essere intero')
+        return
+    if not (isinstance(m, int)):
+        print('m deve essere intero')
+        return
+    if abs(m) > l:
+        print('m deve essere minore in modulo di l')
+        return
+        
+    if m == 0:
+        y =  polinomio_Legendre(l)
+        y.subs(x, cos(theta))
+        return y
+    if m < 0:
+        y = polinomio_Legendre_associato(l,-m)
+        y = (-1)**m*Rational((factorial(l+m)/factorial(l-m))) * y
+        return y
+    
+    y = polinomio_Legendre(l)
+    for i in range(0,m):
+        y = diff(y,x)
+    
+    y = (-1)**m * sympy.sqrt((1-x**2)**(m))*y
+    return y
+
+def armonica_sferica(l,m):
+    y = polinomio_Legendre_associato(l,m)
+    y = sympy.sqrt((2*l+1) * Rational(factorial(l-m)/(4*factorial(l+m))))*y/sympy.sqrt(sympy.pi)
+    y = y.subs(x, cos(theta))
+    y = trigsimp(y)
+    y = simplify(y)
+    y = powsimp(y, force = True)
+    y = y * sympy.exp(sympy.I * m *phi)
+    return y
+
+
+def coefficiente_sviluppo(l,m,r,theta_v,phi_v):
+    z = r**l*armonica_sferica(l,-m)*(-1)**m
+    z = z.subs(theta, theta_v)
+    z = z.subs(phi, phi_v)
+    z = z * 4*sympy.pi/(2*l+1)
+    return z
+
+
+def calcolo_sviluppo(l, punti):
+    sviluppo = x
+    sviluppo = sviluppo - x
+    for i in range(0,l+1):
+        print('Faccio l = %d' %(i))
+        for j in range(-i,i+1):
+            print('Faccio m = %d' %(j))
+            for h in range(0,len(punti)):
+                pota = coefficiente_sviluppo(i,j, punti[h][0], punti[h][1], punti[h][2])
+                ylm = armonica_sferica(i,j)
+                if (not j==0):
+                    sviluppo = sviluppo + punti[h][3]*( 2*re(pota*ylm) )/R**(i+1)
+                else:
+                    sviluppo = sviluppo + punti[h][3]*( pota*ylm ) /R**(i+1)
+    
+    return sviluppo
+    
+    
+
+def output_latex(espressione):
+    scrittura = open(file_out, 'w')
+    scrittura.write('\\documentclass[11pt]{article} \n\\usepackage[T1]{fontenc} \n\\usepackage[utf8]{inputenc} \n')
+    scrittura.write('\\usepackage[italian]{babel} \n\\usepackage[a4paper]{geometry} \n\\usepackage[pdftex]{graphicx} \n')
+    scrittura.write('\\usepackage{amsmath}')
+    scrittura.write('\n\\usepackage{amssymb} \n\\usepackage[T1,OT1]{fontenc}  \n\\title{Espansione in armoniche sferiche fino all\'ordine %d} \n\\begin{document} \n\maketitle \n\n\n' %(ordine))    
+    scrittura.write('\[ %s \] \n' %(latex(espressione)))
+    scrittura.write('\\end{document} \n ')
+    scrittura.close()    
+    
+    
+    
+
+espr = calcolo_sviluppo(ordine, vettore_cariche)
+output_latex(espr)
+
+    
